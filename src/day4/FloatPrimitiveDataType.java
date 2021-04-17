40d64ee0530731736f186e407b0f0c02b8eb3955
Furkat <furkat.khalilov@gmail.com>
2021-04-17 18:16:22 -0400

    Float Primitive Data Types

---
+++ src/day4/FloatPrimitiveDataType.java
@@ -0,0 +1,22 @@
+package day4;
+
+public class FloatPrimitiveDataType {
+
+    public static void main(String[] args) {
+
+        float floatNumberMin = Float.MIN_VALUE;
+        System.out.println("floatNumberMin = " + floatNumberMin);
+
+        float floatNumberMax = Float.MAX_VALUE;
+        System.out.println("floatNumberMax = " + floatNumberMax);
+
+
+        float example = 1.123456789F;
+        System.out.println("example = " + example);
+
+        float example1 = 1.123456789f;
+        System.out.println("example1 = " + example1);
+
+
+    }
+}
