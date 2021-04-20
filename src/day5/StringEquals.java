ce45cb7c6b7697edf81be039b25497e8e49f7e89
furkatalikhalilov <fortestwebsite2021@gmail.com>
2021-04-20 02:37:01 -0400

    test Arzu

--- src/day5/StringEquals.java
+++ src/day5/StringEquals.java
@@ -7,15 +7,14 @@ public class StringEquals {
 
     public static void main(String[] args) {
 
-        String str = "HELLO WORLD";
+        String str = "HELLO WORLD"; // Object ==> String , anObject ==> str
 
         String str2 = "hello world";
 
-        boolean equals = str.equals(str2.toUpperCase());
-        System.out.println("equals = " + equals);
-        boolean equals1 = str.toLowerCase().equals(str2);
-        System.out.println("equals1 = " + equals1);
-
+        boolean equals1 = str.equals(str2.toUpperCase());
+        System.out.println("equals = " + equals1);
+        boolean equals2 = str.toLowerCase().equals(str2);
+        System.out.println("equals1 = " + equals2);
 
 
     }
