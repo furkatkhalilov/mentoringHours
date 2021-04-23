e18d66797156b9ee1af3bb65d13521340f58a4cc
furkatalikhalilov <fortestwebsite2021@gmail.com>
2021-04-23 18:54:20 -0400

    day7 part 2

--- src/day8/ArraysSort.java
+++ src/day8/ArraysSort.java
@@ -15,9 +15,14 @@ public class ArraysSort {
         Arrays.sort(intArray, 1,intArray.length-2);
         System.out.println("intArray = " + Arrays.toString(intArray));
 
-        // create an array of chars.
+        // create an array of chars. {%, f, F, @, 5, a, 7, T, #} and sort starting from char F up to char 7
 
+        char[] charArray = {'%', 'f', 'F', '@', '5','a', '7', 'T', '#'};
 
+//        Arrays.sort(charArray, 2, 6);
+
+        Arrays.sort(charArray,2, charArray.length-3);
+        System.out.println("charArray = " + Arrays.toString(charArray));
 
 
     }
